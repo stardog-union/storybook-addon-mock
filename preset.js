6d2e741c83216d8eb27e008f310023654021aff9
anneeb <annee.barrett@stardog.com>
2023-03-29 11:02:53 -0400

    add preset fiile

---
+++ preset.js
@@ -0,0 +1,12 @@
+function previewAnnotations(entry = []) {
+    return [...entry, require.resolve('./dist/esm/preset/preview')];
+}
+
+function managerEntries(entry = []) {
+    return [...entry, require.resolve('./dist/esm/preset/manager')];
+}
+
+module.exports = {
+    managerEntries,
+    previewAnnotations,
+};
